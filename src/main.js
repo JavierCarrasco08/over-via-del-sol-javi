9079e1812ccb6f11b4d0c8e98c0711de229ce2a0
Javier Carrasco <javierdeveloper08@gmail.com>
2024-02-12 21:58:56 -0400

    change: Cambiando la logica del evento pointerdown

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 const $button__hamburger = document.getElementById('hamburger');
 const $intro__nav = document.querySelector('.intro__nav');
+const $intro__hero = document.querySelector('.intro__hero');
 
-$button__hamburger.addEventListener('pointerdown', () => {
-  $button__hamburger.classList.toggle('active');
-  $intro__nav.classList.toggle('active__nav');
+$intro__hero.addEventListener('pointerdown', (e) => {
+  if (e.target.matches('#hamburger') || e.target.matches('span')) {
+    $button__hamburger.classList.toggle('active');
+    $intro__nav.classList.toggle('active__nav');
+  }
+
+  if (e.target.matches('.intro__a')) {
+    setTimeout(() => {
+      $button__hamburger.classList.toggle('active');
+      $intro__nav.classList.toggle('active__nav');
+    }, 250);
+  }
 });
